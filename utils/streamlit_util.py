4b43ec0e5e7476c3bbd458240abbc2e855741444
BayernMuller <cjjunk29@gmail.com>
2024-02-26 14:59:55 +0900

    feat: add utility functions for handling query parameters

--- utils/streamlit_util.py
+++ utils/streamlit_util.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+from streamlit.components.v1 import html
 
 def remove_streamlit_style():
     st.markdown("""
@@ -9,3 +10,14 @@ def remove_streamlit_style():
     header {visibility: hidden;}
     </style>
     """, unsafe_allow_html=True)
+
+def get_query_params_from_url():
+    params = st.experimental_get_query_params()
+    dict_param = {
+        k: v[0] if isinstance(v, list) else v for k, v in params.items()
+    }
+    return dict_param
+
+def set_query_params_to_url(params: dict[str, str]) -> None:
+    params = {k: v for k, v in params.items() if v}
+    st.experimental_set_query_params(**params)
